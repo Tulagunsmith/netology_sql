4eb3f6e94abe57cfbc8138bd07bfa84f44a390da
Tulagunsmith <tulagunsmith@gmail.com>
2022-08-07 15:08:20 +0300

    initial commit

---
+++ netology_music.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS music_genres (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS musicians (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS genres_musicians (
+	id SERIAL PRIMARY KEY, 
+	genre_id INTEGER NOT NULL REFERENCES music_genres(id),
+	musician_id INTEGER NOT NULL REFERENCES musicians(id)
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(60) NOT NULL,
+	release_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS musicians_albums (
+	id SERIAL PRIMARY KEY, 
+	album_id INTEGER NOT NULL REFERENCES albums(id),
+	musician_id INTEGER NOT NULL REFERENCES musicians(id)
+);
+
+CREATE TABLE IF NOT EXISTS tracks (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(60) NOT NULL,
+	duration NUMERIC NOT NULL,
+	album_id INTEGER NOT NULL REFERENCES albums(id)
+);
+
+CREATE TABLE IF NOT EXISTS collections (
+	id SERIAL PRIMARY KEY, 
+	name VARCHAR(60) NOT NULL,
+	release_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS tracks_collections (
+	id SERIAL PRIMARY KEY, 
+	track_id INTEGER NOT NULL REFERENCES tracks(id),
+	collection_id INTEGER NOT NULL REFERENCES collections(id)
+);
