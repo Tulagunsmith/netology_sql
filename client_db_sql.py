60e429364730d06e43fbaef120b437b9696d9b67
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-10 23:58:49 +0600

    change client data functions added

--- client_db_sql.py
+++ client_db_sql.py
@@ -65,7 +65,7 @@ def create_new_client():
     return client
 
 
-new_client = create_new_client()
+# new_client = create_new_client()
 
 
 def add_new_client(client_name, client_surname, client_phone, client_email):
@@ -98,7 +98,7 @@ def add_new_client(client_name, client_surname, client_phone, client_email):
 
 
 # create_db()
-add_new_client(new_client.name, new_client.surname, new_client.phone, new_client.email)
+# add_new_client(new_client.name, new_client.surname, new_client.phone, new_client.email)
 
 
 def add_phone():
@@ -109,9 +109,79 @@ def add_phone():
             values = ({'phone_number': new_phone_number, 'client_id': person_id})
 
             cur.execute("""
-                        INSERT INTO phones(phone_number, client_id)
-                            VALUES(%(phone_number)s, %(client_id)s)
-                        """, values)
+            INSERT INTO phones(phone_number, client_id)
+                VALUES(%(phone_number)s, %(client_id)s)
+            """, values)
     print('Phone number added.')
 
-add_phone()
+
+# def change_name():
+#     change_data = input("""
+#     What would you like to change? Press 'p' for phone,
+#     'e' for email, 'n' for name or 's' for surname
+#     """)
+#     with psycopg2.connect(database='netology_client_db', user="postgres", password="76239") as conn:
+#         with conn.cursor() as cur:
+#             values = ({f'{change_data}': old_name})
+#
+#             cur.execute("""
+#             SELECT name FROM clients(phone_number, client_id)
+#                 VALUES(%(phone_number)s, %(client_id)s)
+#             """, values)
+
+
+class NewData:
+    def __init__(self):
+        self.old_data = ''
+        self.table = ''
+        self.new_data = ''
+
+
+def change_name(name, person_id):
+    with psycopg2.connect(database='netology_client_db', user="postgres", password="76239") as conn:
+        with conn.cursor() as cur:
+            name_change_query = """UPDATE clients SET name = %s WHERE id =%s"""
+            cur.execute(name_change_query, (name, person_id))
+            conn.commit()
+            print('Name updated!')
+
+
+def change_surname(surname, person_id):
+    with psycopg2.connect(database='netology_client_db', user="postgres", password="76239") as conn:
+        with conn.cursor() as cur:
+            name_change_query = """UPDATE clients SET surname = %s WHERE id =%s"""
+            cur.execute(name_change_query, (surname, person_id))
+            conn.commit()
+            print('Surname updated!')
+
+
+def change_email(email, person_id):
+    with psycopg2.connect(database='netology_client_db', user="postgres", password="76239") as conn:
+        with conn.cursor() as cur:
+            name_change_query = """UPDATE emails SET email_address = %s WHERE id =%s"""
+            cur.execute(name_change_query, (email, person_id))
+            conn.commit()
+            print('Email updated!')
+
+
+def change_phone(phone, phone_id):
+    with psycopg2.connect(database='netology_client_db', user="postgres", password="76239") as conn:
+        with conn.cursor() as cur:
+            name_change_query = """UPDATE phones SET phone_number = %s WHERE id =%s"""
+            cur.execute(name_change_query, (phone, phone_id))
+            conn.commit()
+            print('Phone number updated!')
+
+
+# def change_phone2(phone, phone_id):
+#     name_change_query = """UPDATE phones SET phone_number = %s WHERE id =%s"""
+#     cur.execute(name_change_query, (phone, phone_id))
+#     conn.commit()
+#     print('Phone number updated!')
+#
+#
+# with psycopg2.connect(database='netology_client_db', user="postgres", password="76239") as conn:
+#     with conn.cursor() as cur:
+#         change_phone2(97794444589, 4)
+#         print('Jeronimo!')
+
