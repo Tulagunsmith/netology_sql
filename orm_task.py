9c5daa8cc765485fa1fb2cc358ceb708b7ecb50d
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-24 17:17:20 +0600

    json imported, models filling copied from given example

--- orm_task.py
+++ orm_task.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sq
+import json
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 
@@ -79,7 +80,24 @@ def create_table(engine):
     Base.metadata.create_all(engine)
 
 
-DSN = 'postgresql://postgres:7623@localhost:5432/netology_orm_test'
+DSN = 'postgresql://postgres:76239@localhost:5432/netology_orm_test'
 engine = sq.create_engine(DSN)
 
 create_table(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+with open('fixtures.json', 'r') as fd:
+    data = json.load(fd)
+
+for record in data:
+    model = {
+        'publisher': Publisher,
+        'shop': Shop,
+        'book': Book,
+        'stock': Stock,
+        'sale': Sale,
+    }[record.get('model')]
+    session.add(model(id=record.get('pk'), **record.get('fields')))
+session.commit()
