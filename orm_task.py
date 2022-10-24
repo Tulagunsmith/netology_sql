23f847c841aa83b4dcbf5dea146657c9c9c72466
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-24 12:19:04 +0600

    raw models description added

--- orm_task.py
+++ orm_task.py
@@ -11,6 +11,8 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
 
+    books = relationship("Book", back_populates="publisher")
+
     def __str__(self):
         return f'Publisher {self.id}: {self.name}'
 
@@ -20,6 +22,8 @@ class Shop(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=40), unique=True)
+
+    stocks = relationship("Stock", back_populates="shop")
 
     def __str__(self):
         return f'Shop {self.id}: {self.name}'
@@ -32,7 +36,8 @@ class Book(Base):
     title = sq.Column(sq.String(length=60), unique=True)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
-    publisher = relationship(Publisher, backref="books")
+    publisher = relationship("Publisher", back_populates="books")
+    stocks = relationship("Stock", back_populates="book")
 
     def __str__(self):
         return f'Book {self.id}: {self.title}, {self.id_publisher}'
@@ -46,8 +51,9 @@ class Stock(Base):
     id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    book = relationship(Book, backref="stock")
-    shop = relationship(Shop, backref="stock")
+    book = relationship("Book", back_populates="stocks")
+    shop = relationship("Shop", back_populates="stocks")
+    sales = relationship("Sale", back_populates="stock")
 
     def __str__(self):
         return f'Stock {self.id}: {self.id_book}, {self.id_shop}, {self.count}'
@@ -58,14 +64,14 @@ class Sale(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Float, nullable=False)
-    date_sale = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    date_sale = sq.Column(sq.DateTime, sq.ForeignKey("shop.id"), nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
 
-    book = relationship(Book, backref="stock")
-    shop = relationship(Shop, backref="stock")
+    stock = relationship("Stock", back_populates="sales")
 
     def __str__(self):
-        return f'Stock {self.id}: {self.id_book}, {self.id_shop}, {self.count}'
+        return f'Stock {self.id}: {self.price}, {self.date_sale}, {self.id_stock}, {self.count}'
 
 
 def create_table(engine):
