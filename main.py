3170c2469ee8ebcd8930326b552978d21c4e9c49
Tulagunsmith <tulagunsmith@gmail.com>
2022-10-26 23:03:14 +0600

    final version, dotenv and os imports added, DB data removed

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import os
 import sqlalchemy as sq
 import json
+from dotenv import load_dotenv
 from sqlalchemy.orm import sessionmaker
 from orm_task import create_table, Publisher, Shop, Book, Stock, Sale
 
 
-DSN = 'postgresql://postgres:76239@localhost:5432/netology_orm_test'
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+database = os.getenv('DATABASE')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+
+
+DSN = f'postgresql://{user}:{password}@localhost:5432/{database}'
 engine = sq.create_engine(DSN)
 
 create_table(engine)
@@ -43,3 +54,4 @@ if __name__ == "__main__":
     create_table(engine)
     fill_bd()
     print_publisher()
+
